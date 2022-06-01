609406037bdbd02119aabe0dd19ba7337ed0c32e
Hrishikesh Pathak <hrishi1pathak@gmail.com>
2022-06-01 09:18:34 +0530

    pdf viwer using iframe
    Pdf viewer is added with iframe

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,35 +2,60 @@ import { useState } from "react";
 import * as pdfLib from "pdf-lib";
 import { outlinePdfFactory } from "@lillallol/outline-pdf";
 import filesaver from "file-saver";
+import { PDFDocument, reduceRotation } from "pdf-lib";
 
 export default function Home() {
   const [pdfFileData, setPdfFileData] = useState();
 
-  const onFileSelected = (e) => {
-    const fileList = e.target.files;
-    if (fileList?.length > 0) {
-      console.log(fileList);
-      const reader = new FileReader();
-      reader.readAsArrayBuffer(fileList[0]);
-      reader.addEventListener("load", async (event) => {
-        const result = event.target.result;
-        // Making the outline
-        const outlinepdf = outlinePdfFactory(pdfLib);
-        const outline = `
+  /**
+   * readFileAsync() wraps the FileReader API into a Promise to use as async/await
+   * @param {Blob} file
+   */
+  function readFileAsync(file) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = reject;
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  function renderPdf(arrayBuff) {
+    // const pdfDoc= await PDFDocument.load(arrayBuff);
+    const tempblob = new Blob([new Uint8Array(arrayBuff)], {
+      type: "application/pdf",
+    });
+    const docUrl = URL.createObjectURL(tempblob);
+    setPdfFileData(docUrl);
+  }
+
+  async function OutlinePdf(arrybuff, fileName) {
+    const outlinepdf = outlinePdfFactory(pdfLib);
+    const outline = `
         1||Admit card 1
         2|-|Enei page 2
         3|--|Instruction Page 3
         4|-|Social distancing 4
         5||Self declaration 5
         `;
-        console.log(result);
-        const pdfDocument = await outlinepdf({ outline: outline, pdf: result });
-        const finalpdf = await pdfDocument.save();
-        filesaver.saveAs(
-          new Blob([finalpdf], { type: "application/pdf" }),
-          "modified.pdf"
-        );
-      });
+    console.log(result);
+    const pdfDocument = await outlinepdf({ outline: outline, pdf: arrybuff });
+    const finalpdf = await pdfDocument.save();
+    filesaver.saveAs(
+      new Blob([finalpdf], { type: "application/pdf" }),
+      `${fileName}.pdf`
+    );
+  }
+
+  // Execute when user select a file
+  const onFileSelected = async (e) => {
+    const fileList = e.target.files;
+    if (fileList?.length > 0) {
+      const pdfArrayBuffer = await readFileAsync(fileList[0]);
+      // OutlinePdf(pdfArrayBuffer,"test.pdf");
+      renderPdf(pdfArrayBuffer);
     }
   };
 
@@ -43,6 +68,13 @@ export default function Home() {
         accept=".pdf"
         onChange={onFileSelected}
       />
+      <iframe
+        style={{ display: "block", width: "100vw", height: "90vh" }}
+        title="PdfFrame"
+        src={pdfFileData}
+        frameborder="0"
+        type="application/pdf"
+      ></iframe>
     </>
   );
 }
